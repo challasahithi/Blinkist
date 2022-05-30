3f023c25a961d6f13a44c6637af57969246751a1
Sahithi Challa <sahithi.challa@zemosolabs.com>
2022-05-30 12:14:02 +0530

    Adding actual changes

--- src/components/theme/theme.tsx
+++ src/components/theme/theme.tsx
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Cera Pro",
+    fontFamily: "Cera Pro, sans-serif",
     h1: {
       fontWeight: "bold",
       fontSize: "36px",
